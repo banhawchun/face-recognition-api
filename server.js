654db702f18dc8525050bc96a719f3593cdda7d3
DraGo EmPirE <banhawchun@gmail.com>
2020-02-26 23:46:07 +0700

    update database connection

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
  const db = knex({
  	client: 'pg',
  	 connection: {
- 	 	host: process.env.DATABASE_URL,
+ 	 	host: 'postgresql-globular-37095' //process.env.DATABASE_URL,
  	 	ssl: {
  	 		rejectUnauthorized: true
  	 	},
